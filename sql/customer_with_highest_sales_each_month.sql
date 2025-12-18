a7302e58fbcaa38f3d805fc7f0190a4f290782c8
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-18 09:18:26 -0600

    Add cte query

--- sql/customer_with_highest_sales_each_month.sql
+++ sql/customer_with_highest_sales_each_month.sql
@@ -15,3 +15,28 @@ GROUP BY id, month
 )
   ) WHERE RANK IN (1,2)
 
+-- AS CTE
+WITH monthly_sales AS (
+  SELECT 
+    id as id,
+    month as month,
+    SUM(sales) as total_sales
+  FROM playground.sales 
+GROUP BY id, month
+),
+ranked_sales AS (
+  SELECT 
+    id,
+    month,
+    total_sales,
+    RANK() OVER (PARTITION BY month ORDER BY total_sales desc) as rank  
+    FROM monthly_sales
+)
+SELECT 
+  id,
+  month,
+  total_sales,
+  rank
+FROM ranked_sales
+WHERE RANK IN (1,2)
+ORDER BY month desc
