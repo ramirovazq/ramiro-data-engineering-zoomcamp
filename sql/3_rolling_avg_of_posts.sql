b7fccc8f1c1b73f01ff4a2ed56de1b0b7d907dfd
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-19 07:35:36 -0600

    Add improvement in query as single

--- sql/3_rolling_avg_of_posts.sql
+++ sql/3_rolling_avg_of_posts.sql
@@ -1,18 +1,30 @@
-WITH avg_3_day AS (
-SELECT 
-  user_id,
-  post_date,
-  AVG(post_count) OVER ( 
-    PARTITION BY user_id
-    ORDER BY post_date 
-    ROWS BETWEEN 2 PRECEDING 
-    AND CURRENT ROW
-  ) AS third_rolling_avg
-FROM playground.posts 
-ORDER BY POST_DATE asc
+WITH rolling_avg AS (
+    SELECT 
+    user_id,
+    post_date,
+    AVG(post_count) OVER ( 
+        PARTITION BY user_id
+        ORDER BY post_date 
+        ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ) AS rolling_3_day_avg
+    FROM playground.posts 
 ) 
 SELECT 
   user_id,
   post_date,
-  ROUND(third_rolling_avg, 2) AS third_rolling_avg
-FROM avg_3_day
+  ROUND(rolling_3_day_avg, 2) AS third_rolling_avg
+FROM rolling_avg
+ORDER BY user_id, post_date asc;
+
+-- One single query
+SELECT 
+    user_id,
+    post_date,
+    ROUND(
+        AVG(post_count) OVER ( 
+            PARTITION BY user_id
+            ORDER BY post_date 
+            ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+        ), 2) AS third_rolling_avg
+FROM playground.posts 
+ORDER BY user_id, post_date;
