40405c6d582c4237121a6684237b6cb808cc182e
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-19 07:26:30 -0600

    Add 3 rolling avg

--- sql/3_rolling_avg_of_posts.sql
+++ sql/3_rolling_avg_of_posts.sql
@@ -1,5 +1,18 @@
+WITH avg_3_day AS (
 SELECT 
-    user_id,
-    post_date,
-    AVG(post_count) OVER ( PARTITION BY user_id ORDER BY post_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS third_rolling_avg,
-FROM playground.posts
+  user_id,
+  post_date,
+  AVG(post_count) OVER ( 
+    PARTITION BY user_id
+    ORDER BY post_date 
+    ROWS BETWEEN 2 PRECEDING 
+    AND CURRENT ROW
+  ) AS third_rolling_avg
+FROM playground.posts 
+ORDER BY POST_DATE asc
+) 
+SELECT 
+  user_id,
+  post_date,
+  ROUND(third_rolling_avg, 2) AS third_rolling_avg
+FROM avg_3_day
