aa13820f12eda6673e30781295961299ac053b14
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-19 08:11:39 -0600

    Initial query

---
+++ sql/calculating_available_seats_on_flights.sql
@@ -0,0 +1,33 @@
+-- Calculate the number of seats not yet purchased for each flight. 
+-- Use three tables: 
+--     playground.flights (with columns: flight_id, plane_id)
+--     playground.planes (with columns: plane_id, number_of_seats)
+--     playground.purchases (with columns: flight_id, seat_no)
+-- Each row in purchases is unique for (flight_id, seat_no) pairs. 
+-- For each flight_id, compute the free_seats as the number of seats not purchased. 
+-- Order the results by flight_id in ascending order. 
+-- Ensure consistency in purchases, with no records for non-existing flight_ids or seat_nos.
+
+WITH flight_capacity AS (
+    SELECT 
+        f.flight_id,
+        f.plane_id,    
+        pl.number_of_seats
+    FROM playground.flights f
+    INNER JOIN playground.planes pl
+    ON f.plane_id = pl.plane_id
+), purchased_seats_per_flight AS (
+    SELECT
+        flight_id,
+        COUNT(seat_no) AS purchased_seats
+    FROM playground.purchases
+    GROUP BY flight_id
+)
+SELECT
+    fc.flight_id,
+    --ts.plane_id,
+    fc.number_of_seats - COALESCE(ps.purchased_seats,0) AS free_seats
+FROM flight_capacity fc
+LEFT JOIN purchased_seats_per_flight ps
+ON ts.flight_id = ps.flight_id
+ORDER BY ts.flight_id ASC
