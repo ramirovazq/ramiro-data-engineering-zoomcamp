cc855311d997915f11cb900db7886aa8b4c87f6b
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-22 10:10:04 -0600

    Add best practice solution

--- sql/missing_orders.sql
+++ sql/missing_orders.sql
@@ -25,10 +25,33 @@ SELECT
 FROM(
 SELECT 
   o.order_id,
-  o.customer_id as customer_id,
+  o.customer_id as cust_id,
   o.order_date,
+  c.customer_id as customer_id,
   c.name
 FROM bootcamp.orders o 
 LEFT JOIN bootcamp.customers c
   ON o.customer_id = c.customer_id
 ) WHERE customer_id IS NULL;
+
+-- third approach
+SELECT 
+  o.order_id,
+  o.customer_id,
+  o.order_date
+FROM bootcamp.orders as o 
+LEFT JOIN bootcamp.customers as c
+  ON o.customer_id = c.customer_id
+ WHERE c.customer_id IS NULL
+
+ -- last approach and best practice
+ SELECT
+    o.order_id,
+    o.customer_id,
+    o.order_dateo
+FROM bootcamp.orders o
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM bootcamp.customers as c
+    WHERE o.customer_id = c.customer_id
+);
