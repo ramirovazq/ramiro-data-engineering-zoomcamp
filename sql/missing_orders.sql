4c223c541410dcb48a2699edbe2dce6b1c3a0e2e
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-22 09:36:37 -0600

    Add second option

--- sql/missing_orders.sql
+++ sql/missing_orders.sql
@@ -1,13 +1,23 @@
 
 -- Return the orders with missing customers
+-- Tables
 -- bootcamp.orders
 -- bootcamp.customers
 
-
+-- Answer should include the following columns:
 -- order_id bigint
 -- customer_id bigint
 -- order_date date
 
+-- first approach
+SELECT 
+  o.order_id,
+  o.customer_id,
+  o.order_date
+FROM bootcamp.orders o 
+WHERE customer_id IS NULL;
+
+-- second approach
 SELECT 
   order_id,
   customer_id,
@@ -21,4 +31,4 @@ SELECT
 FROM bootcamp.orders o 
 LEFT JOIN bootcamp.customers c
   ON o.customer_id = c.customer_id
-) WHERE customer_id IS NULL
+) WHERE customer_id IS NULL;
