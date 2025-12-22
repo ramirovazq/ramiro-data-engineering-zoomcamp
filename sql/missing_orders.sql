d244f824aa2821dbe327e9d2d96d562c04ba62c1
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2025-12-22 09:33:43 -0600

    Add simple query

---
+++ sql/missing_orders.sql
@@ -0,0 +1,24 @@
+
+-- Return the orders with missing customers
+-- bootcamp.orders
+-- bootcamp.customers
+
+
+-- order_id bigint
+-- customer_id bigint
+-- order_date date
+
+SELECT 
+  order_id,
+  customer_id,
+  order_date
+FROM(
+SELECT 
+  o.order_id,
+  o.customer_id as customer_id,
+  o.order_date,
+  c.name
+FROM bootcamp.orders o 
+LEFT JOIN bootcamp.customers c
+  ON o.customer_id = c.customer_id
+) WHERE customer_id IS NULL
