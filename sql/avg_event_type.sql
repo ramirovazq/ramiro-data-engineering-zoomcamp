3abd32d29c25b7f8e917698afacaa8b3375288f8
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2026-03-14 13:10:53 -0600

    Refactor

--- sql/avg_event_type.sql
+++ sql/avg_event_type.sql
@@ -11,28 +11,25 @@
 
 WITH average_event_type AS (
     SELECT 
-    event_type, 
-    AVG(occurences) as avg_occurences
+        event_type, 
+        AVG(occurences) as avg_occurences
     FROM playground.business_events be
     GROUP BY be.event_type ),
+
 business_events_with_avg AS (
     SELECT 
-    be.business_id,
-    be.event_type,
-    be.occurences,
-    a.avg_occurences
+        be.business_id,
+        be.event_type,
+        be.occurences,
+        a.avg_occurences
     FROM playground.business_events be
-    LEFT JOIN average_event_type a  
-    ON be.event_type = a.event_type ),
-businesses_with_greater_than_avg AS (
+    JOIN average_event_type a  
+        ON be.event_type = a.event_type )
+
+
     SELECT 
-    bea.business_id,
-    -- bea.event_type,
-    -- bea.occurences,
-    -- bea.avg_occurences,
-    SUM(CASE WHEN bea.occurences > bea.avg_occurences THEN 1 ELSE 0 END) as greather_than_avg
+        bea.business_id
     FROM business_events_with_avg bea
-    GROUP BY bea.business_id)
-SELECT business_id
-FROM businesses_with_greater_than_avg
-WHERE greather_than_avg >= 1;
+    WHERE bea.occurences > bea.avg_occurences
+    GROUP BY bea.business_id
+    HAVING COUNT(*) >= 1;
