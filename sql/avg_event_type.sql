9bc924add3527e4d07a6f8f5d016bb85d17712f2
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2026-03-14 12:49:25 -0600

    Add format to sql

--- sql/avg_event_type.sql
+++ sql/avg_event_type.sql
@@ -10,29 +10,29 @@
 -- business_id with all the business ids that are active.
 
 WITH average_event_type AS (
-SELECT 
+    SELECT 
     event_type, 
     AVG(occurences) as avg_occurences
-FROM playground.business_events 
-GROUP BY event_type ),
+    FROM playground.business_events be
+    GROUP BY be.event_type ),
 business_events_with_avg AS (
-SELECT 
+    SELECT 
     be.business_id,
     be.event_type,
     be.occurences,
     a.avg_occurences
-FROM playground.business_events be
-LEFT JOIN average_event_type a  
-ON be.event_type = a.event_type ),
+    FROM playground.business_events be
+    LEFT JOIN average_event_type a  
+    ON be.event_type = a.event_type ),
 businesses_with_greater_than_avg AS (
-SELECT 
-bea.business_id,
--- bea.event_type,
--- bea.occurences,
--- bea.avg_occurences,
-SUM(CASE WHEN bea.occurences > bea.avg_occurences THEN 1 ELSE 0 END) as greather_than_avg
-FROM business_events_with_avg bea
-GROUP BY 1)
+    SELECT 
+    bea.business_id,
+    -- bea.event_type,
+    -- bea.occurences,
+    -- bea.avg_occurences,
+    SUM(CASE WHEN bea.occurences > bea.avg_occurences THEN 1 ELSE 0 END) as greather_than_avg
+    FROM business_events_with_avg bea
+    GROUP BY bea.business_id)
 SELECT business_id
 FROM businesses_with_greater_than_avg
 WHERE greather_than_avg >= 1;
