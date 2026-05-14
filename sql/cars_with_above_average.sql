a2d601aff0aedeee065ff79ba1c828663c0a63b6
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2026-05-14 06:20:44 -0600

    Add sql for avg cars

---
+++ sql/cars_with_above_average.sql
@@ -0,0 +1,55 @@
+-- Cars with Above Average Engine Size
+ 
+
+-- Using the table playground.automobile, Create a SQL query to identify cars that have an engine size above the average across all cars in the dataset. The result should include the brand, fuel_type, and engine size, ordered by engine size in descending order and then brand_name in asc order.
+-- These are the tables to query for this question:
+-- playground.automobile
+
+--     brand_name string
+--     fuel_type string
+--     aspiration string
+--     door_panel string
+--     design string
+--     wheel_drive string
+--     engine_location string
+--     engine_type string
+--     cylinder_count string
+--     engine_size int
+--     fuel_system string
+--     bore double
+--     stroke double
+--     compression_ratio double
+--     horse_power int
+--     top_RPM int
+--     city_mileage int
+--     highway_mileage int
+--     price_in_dollars int
+
+-- Your answer should include these columns:
+
+--     brand_name varchar
+--     fuel_type varchar
+--     engine_size integer
+
+
+SELECT
+  brand_name,
+  fuel_type,
+  AVG(engine_size) average_engine_size
+FROM playground.automobile 
+GROUP BY 1,2;
+
+WITH average_engine_size AS (
+  SELECT
+    AVG(engine_size) OVER(PARTITION BY brand_name, fuel_type) as avg_engine_size,
+    brand_name,
+    fuel_type,
+    engine_size
+  FROM playground.automobile 
+)
+SELECT 
+  brand_name,
+  fuel_type,
+  engine_size
+FROM average_engine_size
+WHERE engine_size > avg_engine_size
